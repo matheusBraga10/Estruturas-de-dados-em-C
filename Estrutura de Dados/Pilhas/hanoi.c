bd117a4066cce4edf9081fcdefb327e31b9ec1a0
Matheus Felipe Braga <matheusfbraga10@gmail.com>
2023-05-16 23:54:49 -0300

    16-05

--- Estrutura de Dados/Pilhas/hanoi.c
+++ Estrutura de Dados/Pilhas/hanoi.c
@@ -11,5 +11,39 @@ typedef struct pilha
 typedef struct lista
 {
     int nome;
-    pilha 
+    struct lista *proximo;
+    pilha hanoi;
 }lista;
+
+void inicializa(lista *l, int valor){
+    l->nome = 0;
+    l->proximo = NULL;
+    int contador = valor;
+    for(int i=0; i < valor; i++){
+        pilha *novo = (pilha *)malloc(sizeof(pilha));
+        novo->proximo = NULL;
+        l->hanoi.num = valor;
+        valor--;
+        l->hanoi.proximo = novo;
+        novo->proximo = NULL;
+        l->hanoi.proximo = l->hanoi.proximo->proximo;
+    }
+}
+
+
+void criaHanoi(lista *l, pilha *p, int valor){
+    for(int i = 0; i < valor-1; i++){
+        lista *aux = (lista *)malloc(sizeof(lista));
+        aux->nome = i+1;
+        aux->proximo = NULL;
+        l->proximo = aux;
+        l->proximo = l->proximo->proximo; 
+    }
+}
+
+
+
+int main(){
+
+    return 0;
+}
